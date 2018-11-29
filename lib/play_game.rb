b8741882d76fef7f7bd01308d6678059977268d2
Rupa Dhillon <rupa211984@gmail.com>
2018-11-29 15:56:42 -0800

    Get input for Player class. No real changes for play_game.rb

---
+++ lib/play_game.rb
@@ -0,0 +1,3 @@
+# Start the game off by using necessary classes
+
+require_relative './player.rb'
---
+++ lib/player.rb
@@ -0,0 +1,35 @@
+# Handle basic Player functions for Ghost: get valid name and input for game
+
+class Player
+  attr_reader :name
+  def initialize(name)
+    @name = valid_name(name)
+  end
+
+  def valid_name(name)
+    test_name = name
+    while test_name == ''
+      puts 'Please enter a valid name.'
+      test_name = gets.chomp
+    end
+
+    test_name.capitalize
+  end
+
+  def guess(str)
+    letter = str.to_s
+    while /[a-zA-z]/.match(letter) == nil || letter.length != 1
+      puts 'Please enter a valid letter from A to Z.'
+      letter = gets.chomp
+    end
+
+    letter.downcase
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  player = Player.new('')
+  puts player.name
+  #player.guess(1)
+  player.guess('i')
+end
