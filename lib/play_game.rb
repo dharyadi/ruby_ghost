bc07f660eb44b8625df8a9c4da795197e0240275
Rupa Dhillon <rupa211984@gmail.com>
2018-11-29 17:19:39 -0800

    Add welcome message, get players

--- lib/play_game.rb
+++ lib/play_game.rb
@@ -1,3 +1,14 @@
 # Start the game off by using necessary classes
 
 require_relative './player.rb'
+require_relative './game.rb'
+
+# Welcome and get player number
+puts 'Welcome to GHOST! Ooh, it’s spooooooky!'
+puts
+puts 'How many players?'
+ghost = Game.new(gets.chomp.to_i)
+
+# Show who's playing
+player_names = ghost.players.map { |player| player.name }
+puts "Playing today, we have: #{player_names.join(', ')}"
