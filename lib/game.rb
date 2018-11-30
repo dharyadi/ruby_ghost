e0c51535752fcbb91f66a52dcaa4f46e816cf2bf
Rupa Dhillon <rupa211984@gmail.com>
2018-11-29 16:45:51 -0800

    Create valid players

---
+++ lib/game.rb
@@ -0,0 +1,35 @@
+# All game functions for turns, rounds, wins, losses
+require_relative './player.rb'
+
+class Game
+  attr_reader :players
+  @@MIN_PLAYERS = 2
+  @@MAX_PLAYERS = 4
+
+  def initialize(num_players)
+    num_players = validate_num_players(num_players)
+    @players = []
+    create_players(num_players)
+  end
+
+  def validate_num_players(num_players)
+    while !(@@MIN_PLAYERS..@@MAX_PLAYERS).include?(num_players)
+      puts "Please enter a number between #{@@MIN_PLAYERS} and #{@@MAX_PLAYERS}."
+      num_players = gets.chomp.to_i
+    end
+    num_players
+  end
+
+  def create_players(num_players)
+    num_players.times do |num|
+      puts "Player #{num + 1} name:"
+      @players << Player.new(gets.chomp)
+    end
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  #game = Game.new('cat')
+  game = Game.new(3)
+  p game.players
+end
